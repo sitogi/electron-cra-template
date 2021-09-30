0d488645b2d5ef25fce0f4359db4488d7842814f
sitogi <takano162@gmail.com>
2021-09-30 21:31:52 +0900

    Update misc

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-const { BrowserWindow, app } = require('electron');
+import { app, BrowserWindow } from 'electron';
 
-const createWindow = () => {
-  const mainWindow = new BrowserWindow();
-  mainWindow.loadURL('http://localhost:8000');
+const createWindow = async () => {
+  const mainWindow = new BrowserWindow({ width: 1200, height: 780 });
+  await mainWindow.loadURL('http://localhost:8000');
 };
 
 app.whenReady().then(createWindow);
 app.once('window-all-closed', () => app.quit());
-
