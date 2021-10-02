dd93d2784f2d112a5531456d462b87f85d0494b0
sitogi <takano162@gmail.com>
2021-10-02 21:37:58 +0900

    Update and add some packages

---
+++ electron/main.ts
@@ -0,0 +1,39 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from "path";
+import * as isDev from 'electron-is-dev';
+
+const createWindow = async () => {
+  // Crate the browser window
+  const mainWindow = new BrowserWindow({ width: 1200, height: 1000 });
+  if (isDev) {
+    await mainWindow.loadURL("http://localhost:8000/index.html");
+  } else {
+    // 'build/index.html'
+    await mainWindow.loadURL(`file://${__dirname}/../index.html`);
+  }
+
+  // Hot Reloading
+  if (isDev) {
+    // 'node_modules/.bin/electronPath'
+    require("electron-reload")(__dirname, {
+      electron: path.join(
+        __dirname,
+        "..",
+        "..",
+        "node_modules",
+        ".bin",
+        "electron"
+      ),
+      forceHardReset: true,
+      hardResetMethod: "exit",
+    });
+  }
+
+  // Open the DevTools.
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+};
+
+app.whenReady().then(createWindow);
+app.once('window-all-closed', () => app.quit());
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { app, BrowserWindow } from 'electron';
-
-const createWindow = async () => {
-  const mainWindow = new BrowserWindow({ width: 1200, height: 780 });
-  await mainWindow.loadURL('http://localhost:8000');
-};
-
-app.whenReady().then(createWindow);
-app.once('window-all-closed', () => app.quit());
